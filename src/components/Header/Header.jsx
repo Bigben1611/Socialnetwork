eda2923c93ac16f0cc6a2a33914558b48e129870
Bigben1611 <kutumov.sz@gmail.com>
2023-05-27 11:06:54 +0500

    Complete header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Styles from "./Header.module.scss"
+import logo from './logo.svg';
+
 
 const Header = (props) => {
     return (
       <div {...props} className={props.className+" "+Styles.Header}>
-          Header
+          <img src={logo} alt="logo" />
       </div>
     );
 }
